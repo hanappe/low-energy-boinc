aca0ffdd1a9b12993fb909054bfb958db90588ec
KevReCo <kevin.renecorail@gmail.com>
2014-03-31 17:02:10 +0200

    remove .d file

--- boincapps/low-energy-boinc/src/sensors/ArduinoTemp.d
+++
@@ -1,5 +0,0 @@
-../sensors/ArduinoTemp.d ../sensors/ArduinoTemp.o: \
- ../sensors/ArduinoTemp.cpp ../sensors/ArduinoTemp.hpp \
- ../sensors/SensorManager.hpp ../sensors/Sensor.hpp \
- ../sensors/Datapoint.hpp ../sensors/Error.hpp \
- ../../depends/serial/serial.h
