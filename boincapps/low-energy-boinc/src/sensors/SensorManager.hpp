34a65b7c37c1d39ed926c08b1677a42c277f03e6
KevReCo <kevin.renecorail@gmail.com>
2014-04-23 17:04:48 +0200

    add logger

--- boincapps/low-energy-boinc/src/sensors/SensorManager.hpp
+++ boincapps/low-energy-boinc/src/sensors/SensorManager.hpp
@@ -11,8 +11,22 @@ struct SensorManager {
 		// Add SensorManager's sensors to sensors_out
         virtual void add_sensors(SensorV& sensors_out, ErrorV& errors) = 0;
         virtual void update_sensors() = 0;
+        
+        void fileLog(const std::string& msg) {
+                
+                std::ofstream f("sensor.log", std::ios_base::app );
+                if (f) {
+                        std::stringstream s;
+                        f << "[";
+                        Time::print_format(f, Time::get_current());
+                        f << "]";
+                        f << "[" << m_name << "] ";
+                        f << msg;
+                        f << std::endl;
+                        f.close();
+                }
 
-		inline Logger FileLog() { return Logger(m_name); }
+        }
 };
 
 #endif
